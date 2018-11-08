efc874cf65795476a910a0eb93029c6600f3947c
lee kwangsu <me@korea.kr>
2018-11-08 22:47:40 +0900

    login logout added

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('accounts/login/', LoginView.as_view(), name='login' ),
+    path('accounts/logout/',LogoutView.as_view(), name='logout' ),
     url(r'', include('blog.urls')),
 ]
